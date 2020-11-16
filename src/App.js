94af79edf514585b5352f25faea859a562f6c146
usmangillpro <usmangillpro@gmail.com>
2020-11-16 21:18:24 +0500

    Search widget added functional using useEffect

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Accordion from "./components/Accordion";
+import Search from "./components/Search";
 
 const items = [
   {
@@ -20,7 +21,8 @@ const App = () => {
   return (
     <div>
       <br />
-      <Accordion items={items} />
+      <Search />
+      {/* <Accordion items={items} /> */}
     </div>
   );
 };
--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,23 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Accordion = ({ items }) => {
+  const [activeIndex, setActiveIndex] = useState(null);
   const onTitleClick = (index) => {
-    console.log("Title was clicked", index);
+    setActiveIndex(index);
   };
   const renderedItems = items.map((item, index) => {
+    const active = index === activeIndex ? "active" : "";
     return (
       <React.Fragment key={item.title}>
-        <div onClick={() => onTitleClick(index)} className="title active">
+        <div onClick={() => onTitleClick(index)} className={`title ${active}`}>
           <i className="dropdown icon"></i>
           {item.title}
         </div>
-        <div className="content active">
+        <div className={`content ${active}`}>
           <p>{item.content}</p>
         </div>
       </React.Fragment>
     );
   });
-  return <div className="ui stylled accordion"> {renderedItems}</div>;
+  return (
+    <div className="ui stylled accordion">
+      {renderedItems}
+      <h1>{activeIndex}</h1>
+    </div>
+  );
 };
 
 export default Accordion;
